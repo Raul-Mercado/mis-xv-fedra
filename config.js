5343035edd2fd41896bd41dc7798676001975e2c
Raul-Mercado <114114675+Raul-Mercado@users.noreply.github.com>
2026-08-30 21:38:31 -0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ const CONFIG = {
   fotoFondo: "fondo-fedra.jpg",
 
   // --- Música de fondo (URL de un mp3) ---
-  musicaUrl: "ChristinaPerri-AThousandYears",
+  musicaUrl: "ChristinaPerri-AThousandYears.mp3",
 
   // --- Regalo en dinero (Mercado Pago) ---
   aliasMercadoPago: "fedra.ferrero.",
